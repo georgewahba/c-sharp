fb6943c0ac217dfc70cf6f1a1a03a986b63cc733
Steven <steven_x01@hotmail.com>
2023-11-01 22:10:08 +0100

    rekenmachine uit de les
    Echt precies die les oplossing gekopieerd als basis

---
+++ beheerapplicatie/NotifyPropertyChanged.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace RekenmachineCollege4
+{
+    // Dit is stap 2
+    class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisePropertyChange(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+    }
+}
