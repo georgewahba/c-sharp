34c9ecaf55302b2705a2f33e593d6048b45d5472
George Wahba <54804560+georgewahba@users.noreply.github.com>
2023-11-15 10:43:17 +0100

    create playlist model

--- beheerapplicatie/models/Song.cs
+++ beheerapplicatie/models/Song.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace beheerapplicatie.models
 {
-    internal class Song
+    public class Song
     {
         public int Id { get; set; }
         public string Title { get; set; }
