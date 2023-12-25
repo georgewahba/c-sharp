c4d1c308afd79567f62cb1c7fcd0354000f99849
George Wahba <54804560+georgewahba@users.noreply.github.com>
2023-12-25 14:50:08 +0100

    lochica van Basemodelview

---
+++ beheerapplicatie/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beheerapplicatie
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Serialize<T>(string fileName, ObservableCollection<T> collection)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(collection, Formatting.Indented);
+                File.WriteAllText(fileName, json);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+    }
+}
