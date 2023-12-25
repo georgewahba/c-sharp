a5e59396a8571e7e2d7ba217b95f1f8de98455ef
George Wahba <54804560+georgewahba@users.noreply.github.com>
2023-12-25 15:03:01 +0100

    relaycommand met interface ICommand

---
+++ beheerapplicatie/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace beheerapplicatie
+{
+    public class RelayCommand : ICommand
+    {
+        //RelayCommand class om de commands te kunnen gebruiken
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+        
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(Execute));
+            this.canExecute = canExecute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.CanExecute(parameter);
+        }
+        
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
+
