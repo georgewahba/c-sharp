c1124968a5bd512ffeb83d66e856c8f114c66507
George Wahba <54804560+georgewahba@users.noreply.github.com>
2023-12-25 16:30:54 +0100

    buildfiles meesturen

--- beheerapplicatie/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ beheerapplicatie/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace beheerapplicatie {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/beheerapplicatie;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/beheerapplicatie;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
