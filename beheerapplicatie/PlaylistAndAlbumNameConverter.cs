ec5610bc99553c0acfb07a0cfa7f9fe790604954
George Wahba <54804560+georgewahba@users.noreply.github.com>
2023-12-25 14:55:14 +0100

    voor playlist en album een naam converter gemaakt  met IValueConverter interface en multiconverter met IValueConverter interface

---
+++ beheerapplicatie/PlaylistAndAlbumNameConverter.cs
@@ -0,0 +1,31 @@
+﻿using beheerapplicatie.models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace beheerapplicatie
+{
+    public class PlaylistAndAlbumNameConverter : IValueConverter
+    {
+        //Converter die de namen van de songs in een playlist of album omzet naar een lijst van strings
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            List<string> names = new List<string>();
+            if (value is ObservableCollection<Song>)
+            {
+                names = ((ObservableCollection<Song>)value).Select(s => s.Name).ToList();
+            }
+            return names;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ beheerapplicatie/PlaylistAndAlbumNameMultiConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace beheerapplicatie
+{
+
+    public class PlaylistAndAlbumNameMultiConverter : IMultiValueConverter
+    {
+        //Converter die een array van objecten teruggeeft met de naam van de playlist en de naam van het album
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.Clone();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
