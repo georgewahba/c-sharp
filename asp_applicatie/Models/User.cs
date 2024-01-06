0f62cbc17fa9dd3426d7ce29a629019c18723f0f
Steven <steven_x01@hotmail.com>
2024-01-07 00:17:45 +0100

    models toegevoegd
    models opnieuw erin

---
+++ asp_applicatie/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace asp_applicatie.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? FullName { get; set; }
+
+    public int? LastPlayedSongId { get; set; }
+
+    public int? Status { get; set; }
+
+    public Song? LastPlayedSong { get; set; }
+
+}
