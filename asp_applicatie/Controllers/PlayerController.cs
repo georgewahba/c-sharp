dc913867265a2425af064777fe71b0d710db9b7a
Steven <steven_x01@hotmail.com>
2024-01-07 00:58:54 +0100

    Create PlayerController.cs

---
+++ asp_applicatie/Controllers/PlayerController.cs
@@ -0,0 +1,107 @@
+﻿using asp_applicatie.Models;
+using asp_applicatie.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp_applicatie.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly AspApplicatieDbContext _context;
+
+        public PlayerController(AspApplicatieDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int? id, int PLId, int AlbumId)
+        {
+            if (PLId != 0)
+            {
+                // Retrieve playlist information
+                var viewModel = await _context.Playlists
+           .Where(p => p.PlaylistId == PLId)
+           .Select(p => new PlayListSongViewModel
+           {
+               PlaylistId = p.PlaylistId,
+               PlaylistName = p.Name,
+               songs = p.PlaylistSongs!.Select(ps => new SongViewModel
+               {
+                   SongId = ps.Song!.SongId,
+                   SongTitle = ps.Song.Title,
+                   AudioFilePath = ps.Song.AudioFilePath,
+                   Ratings = ps.Song.Ratings,
+                   Duration = ps.Song.Duration
+               }).ToList()
+           })
+           .FirstOrDefaultAsync();
+
+                if (viewModel == null)
+                {
+                    return NotFound();
+                }
+                ViewBag.SongId = id;
+                return View(viewModel);
+            }
+            else if (AlbumId != 0)
+            {
+                var viewModel = await _context.Albums
+           .Where(p => p.AlbumId == AlbumId)
+           .Select(p => new PlayListSongViewModel
+           {
+               PlaylistId = 0,
+               PlaylistName = "Album",
+               songs = p.Songs!.Select(ps => new SongViewModel
+               {
+                   SongId = ps.SongId,
+                   SongTitle = ps.Title,
+                   AudioFilePath = ps.AudioFilePath,
+                   Ratings = ps.Ratings,
+                   Duration = ps.Duration
+               }).ToList()
+           })
+           .FirstOrDefaultAsync();
+                ViewBag.SongId = 1;
+                return View(viewModel);
+            }
+            else
+            {
+                PlayListSongViewModel playlistSong = new PlayListSongViewModel();
+                if (id == 0)
+                {
+                    id = GeneralInformation.LastPlayedSongId;
+                }
+                var song = await _context.Songs
+                                .FirstOrDefaultAsync(m => m.SongId == id);
+                if (song != null)
+                {
+                    playlistSong = new PlayListSongViewModel
+                    {
+                        PlaylistId = 0,
+                        PlaylistName = "Random",
+                        songs = new List<SongViewModel>
+                    {
+                        new SongViewModel
+                        {
+                            SongId = song.SongId,
+                            SongTitle = song.Title,
+                            AudioFilePath = song.AudioFilePath,
+                            Ratings = song.Ratings,
+                            Duration = song.Duration
+                        }
+                    }
+                    };
+                }
+
+                ViewBag.SongId = id;
+                return View(playlistSong);
+            }
+        }
+        //[HttpPost]
+        //public IActionResult AddToQueue(int id, int albumId, int playlistId)
+        //{
+        //    MusicPlayer.Queue.Add(new QueueItem { SongId = id, AlbumId = albumId, PlaylistId = playlistId });
+        //    // Redirect back to the original view or any other appropriate action
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
