52f146369dc5deca7d3fe9b4b7bfa0151a85001b
Steven <steven_x01@hotmail.com>
2024-01-07 01:04:06 +0100

    controllers aangepast
    generalinformation file aangemaakt

--- asp_applicatie/Controllers/PlaylistController.cs
+++ asp_applicatie/Controllers/PlaylistController.cs
@@ -1,107 +1,162 @@
-﻿using asp_applicatie.Models;
-using asp_applicatie.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using asp_applicatie.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using asp_applicatie.ViewModels;
 
 namespace asp_applicatie.Controllers
 {
-    public class PlayerController : Controller
+    public class PlaylistController : Controller
     {
         private readonly AspApplicatieDbContext _context;
 
-        public PlayerController(AspApplicatieDbContext context)
+        public PlaylistController(AspApplicatieDbContext context)
         {
             _context = context;
         }
-        public async Task<IActionResult> Index(int? id, int PLId, int AlbumId)
+
+        // GET: Playlist
+        public async Task<IActionResult> Index()
         {
-            if (PLId != 0)
-            {
-                // Retrieve playlist information
-                var viewModel = await _context.Playlists
-           .Where(p => p.PlaylistId == PLId)
-           .Select(p => new PlayListSongViewModel
-           {
-               PlaylistId = p.PlaylistId,
-               PlaylistName = p.Name,
-               songs = p.PlaylistSongs!.Select(ps => new SongViewModel
-               {
-                   SongId = ps.Song!.SongId,
-                   SongTitle = ps.Song.Title,
-                   AudioFilePath = ps.Song.AudioFilePath,
-                   Ratings = ps.Song.Ratings,
-                   Duration = ps.Song.Duration
-               }).ToList()
-           })
-           .FirstOrDefaultAsync();
+            var userId = GeneralInformation.UId;
 
-                if (viewModel == null)
+            var viewModel = await _context.Playlists
+                .Where(p => !p.IsPrivate || (p.IsPrivate && p.UserId == userId))
+                .Select(p => new PlaylistViewModel
                 {
-                    return NotFound();
-                }
-                ViewBag.SongId = id;
-                return View(viewModel);
-            }
-            else if (AlbumId != 0)
-            {
-                var viewModel = await _context.Albums
-           .Where(p => p.AlbumId == AlbumId)
-           .Select(p => new PlayListSongViewModel
-           {
-               PlaylistId = 0,
-               PlaylistName = "Album",
-               songs = p.Songs!.Select(ps => new SongViewModel
-               {
-                   SongId = ps.SongId,
-                   SongTitle = ps.Title,
-                   AudioFilePath = ps.AudioFilePath,
-                   Ratings = ps.Ratings,
-                   Duration = ps.Duration
-               }).ToList()
-           })
-           .FirstOrDefaultAsync();
-                ViewBag.SongId = 1;
-                return View(viewModel);
-            }
-            else
-            {
-                PlayListSongViewModel playlistSong = new PlayListSongViewModel();
-                if (id == 0)
-                {
-                    id = GeneralInformation.LastPlayedSongId;
-                }
-                var song = await _context.Songs
-                                .FirstOrDefaultAsync(m => m.SongId == id);
-                if (song != null)
-                {
-                    playlistSong = new PlayListSongViewModel
-                    {
-                        PlaylistId = 0,
-                        PlaylistName = "Random",
-                        songs = new List<SongViewModel>
-                    {
-                        new SongViewModel
-                        {
-                            SongId = song.SongId,
-                            SongTitle = song.Title,
-                            AudioFilePath = song.AudioFilePath,
-                            Ratings = song.Ratings,
-                            Duration = song.Duration
-                        }
-                    }
-                    };
-                }
+                    PlaylistId = p.PlaylistId,
+                    Name = p.Name,
+                    IsPrivate = p.IsPrivate,
+                    User = p.User
+                })
+                .ToListAsync();
 
-                ViewBag.SongId = id;
-                return View(playlistSong);
-            }
+            return View(viewModel);
         }
-        //[HttpPost]
-        //public IActionResult AddToQueue(int id, int albumId, int playlistId)
-        //{
-        //    MusicPlayer.Queue.Add(new QueueItem { SongId = id, AlbumId = albumId, PlaylistId = playlistId });
-        //    // Redirect back to the original view or any other appropriate action
-        //    return RedirectToAction("Index");
-        //}
+        // GET: Playlist/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Playlists == null)
+            {
+                return NotFound();
+            }
+
+            var playlist = await _context.Playlists
+                .FirstOrDefaultAsync(m => m.PlaylistId == id);
+            if (playlist == null)
+            {
+                return NotFound();
+            }
+
+            return View(playlist);
+        }
+
+        // GET: Playlist/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Playlist/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PlaylistId,Name,IsPrivate,UserId,Status")] Playlist playlist)
+        {
+            playlist.Status = 1;
+            if (ModelState.IsValid)
+            {
+                _context.Add(playlist);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(playlist);
+        }
+
+        // GET: Playlist/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (_context.Playlists == null)
+            {
+                return NotFound();
+            }
+
+            var playlist = await _context.Playlists.FindAsync(id);
+
+            int userId = GeneralInformation.UId;
+            ViewBag.UserId = userId;
+            return View(playlist);
+        }
+
+        // POST: Playlist/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Save([Bind("PlaylistId,Name,IsPrivate,UserId,Status")] Playlist playlist)
+        {
+            playlist.Status = 1;
+
+            if (playlist.PlaylistId == 0) // PlaylistId is zero, add a new entry
+            {
+                _context.Add(playlist);
+            }
+            else // PlaylistId is non-zero, update the existing entry
+            {
+                _context.Update(playlist);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        // GET: Playlist/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Playlists == null)
+            {
+                return NotFound();
+            }
+
+            var playlist = await _context.Playlists
+                .FirstOrDefaultAsync(m => m.PlaylistId == id);
+            if (playlist == null)
+            {
+                return NotFound();
+            }
+
+            return View(playlist);
+        }
+
+        // POST: Playlist/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Playlists == null)
+            {
+                return Problem("Entity set 'AspApplicatieDbContext.Playlists'  is null.");
+            }
+            var playlist = await _context.Playlists.FindAsync(id);
+            if (playlist != null)
+            {
+                _context.Playlists.Remove(playlist);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlaylistExists(int id)
+        {
+            return (_context.Playlists?.Any(e => e.PlaylistId == id)).GetValueOrDefault();
+        }
     }
 }
---
+++ asp_applicatie/GeneralInformation.cs
@@ -0,0 +1,9 @@
+﻿namespace asp_applicatie
+{
+    public static class GeneralInformation
+    {
+        public static int UId = 0;
+        public static int? LastPlayedSongId = 0;
+        public static string? UserName = "";
+    }
+}
