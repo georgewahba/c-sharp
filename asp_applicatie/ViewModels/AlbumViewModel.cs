bc9bf6d775124abffe0a7199dd210ddcbf353727
Steven <steven_x01@hotmail.com>
2024-04-08 12:02:45 +0200

    geen duplicate code

--- asp_applicatie/ViewModels/AlbumViewModel.cs
+++ asp_applicatie/ViewModels/AlbumViewModel.cs
@@ -1,15 +1,9 @@
-﻿namespace asp_applicatie.ViewModels
+﻿using asp_applicatie.Models;
+
+namespace asp_applicatie.ViewModels
 {
-    public class AlbumViewModel
+    public class AlbumViewModel : Album
     {
-        public int AlbumId { get; set; }
 
-        public string? Title { get; set; }
-
-        public int ArtistId { get; set; }
-
-        public DateTime? ReleaseDate { get; set; }
-
-        public int? Quantity { get; set; }
     }
 }
--- asp_applicatie/ViewModels/SongViewModel.cs
+++ asp_applicatie/ViewModels/SongViewModel.cs
@@ -2,14 +2,9 @@
 
 namespace asp_applicatie.ViewModels
 {
-    public class SongViewModel
+    public class SongViewModel : Song
     {
-        public int SongId { get; set; }
         public string? SongTitle { get; set; }
-        public string? AudioFilePath { get; set; }
-        public int? Duration { get; set; }
-        public int? Ratings { get; set; }
 
-        public Album? album { get; set; }
     }
 }
